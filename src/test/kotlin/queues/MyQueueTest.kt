95963b0677451f8dd2320df7ba7ab841b6f8a390
tobialbert <daadatobi@gmail.com>
2022-11-12 23:14:40 +0000

    Test: add setup method.

--- src/test/kotlin/queues/MyQueueTest.kt
+++ src/test/kotlin/queues/MyQueueTest.kt
@@ -1,21 +1,25 @@
 package queues
 
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
 
 class MyQueueTest {
 
+    private lateinit var queue: MyQueue<Int>
+
+    @Before
+    fun setup() {
+        queue = MyQueue()
+    }
+
     @Test
     fun `queue initialized with no elements has first node set to null`() {
-        val queue = MyQueue<Int>()
-
         assertNull(queue.first)
     }
 
     @Test
     fun `queue initialized with no elements has last node set to null`() {
-        val queue = MyQueue<Int>()
-
         assertNull(queue.last)
     }
 }
