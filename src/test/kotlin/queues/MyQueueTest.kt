b8a32249a8d2861f9236b0379a7ff5bd77491723
tobialbert <daadatobi@gmail.com>
2022-11-12 23:07:21 +0000

    Test: queue initialized with no elements has first and last nodes set to null

--- src/test/kotlin/queues/MyQueueTest.kt
+++ src/test/kotlin/queues/MyQueueTest.kt
@@ -1,5 +1,21 @@
 package queues
 
 import org.junit.Assert.*
+import org.junit.Test
 
-class MyQueueTest
+class MyQueueTest {
+
+    @Test
+    fun `queue initialized with no elements has first node set to null`() {
+        val queue = MyQueue<Int>()
+
+        assertNull(queue.first)
+    }
+
+    @Test
+    fun `queue initialized with no elements has last node set to null`() {
+        val queue = MyQueue<Int>()
+
+        assertNull(queue.last)
+    }
+}
